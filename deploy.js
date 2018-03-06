4a4edbc3c8a64e5f99cce7b84a9fdc1c02413f27
jdm79 <james.malvern@gmail.com>
2018-03-06 20:08:38 +0000

    add test to check if an account can enter

--- deploy.js
+++ deploy.js
@@ -17,7 +17,7 @@ const deploy = async () => {
   // interface is the ABI
   const result = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({ data: bytecode })
-    .send({ gas: '500000', from: accounts[0] })
+    .send({ gas: '1000000', from: accounts[0] })
 
   console.log('our contract was deployed to ', result.options.address)
 }
